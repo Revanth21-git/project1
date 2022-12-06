09810a3c01635b61b44f2a46c99d6e2299f9114b
Revanth21-git <revanth349@gmail.com>
2022-12-06 11:45:19 +0530

    3 methods added

--- src/test/java/org/hexa/GitTask.java
+++ src/test/java/org/hexa/GitTask.java
@@ -10,6 +10,18 @@ public class GitTask {
 		System.out.println("method1");
 
 	}
+	public void method2() {
+		System.out.println("method2");
+		
+	}
+	public void method3() {
+		System.out.println("method3");
+		
+	}
+	public void method4() {
+		System.out.println("method4");
+		
+	}
 	
 	
 	
